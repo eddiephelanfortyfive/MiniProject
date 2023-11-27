39d1ce49c3f28c99af36277fc97680fd91679d33
eddiephelanfortyfive <23397179@studentmail.ul.ie>
2023-11-27 14:42:56 +0000

    question picker modified

--- RandomQuestions.java
+++ RandomQuestions.java
@@ -6,33 +6,52 @@ import java.util.Scanner;
 
 public class RandomQuestions {
 	private static final int QUESTIONS_PER_ROUND = 2;
-    private static final int MAXIMUM_QUESTIONS = 5;
 
-	public int RondomiseQuesionts(int score,Difficulty diff, ArrayList<Question> Questions, Scanner scanner) {
+	public boolean isEqual(ArrayList<Integer> previousNum, int questionNumber) {
+		boolean result = false;
+		for (int i = 0; i < previousNum.size(); i++) {
+			if (previousNum.get(i) == questionNumber) {
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	public int RondomiseQuesionts(int score, Difficulty diff, ArrayList<Question> Questions, Scanner scanner) {
 		String roundNum = "---------------------------------------------------------------------------------------------------------\n"
-                + "                                             "+diff + "  LEVEL\n"
-                + "---------------------------------------------------------------------------------------------------------\n";
-        UIHelper newRound = new UIHelper();
-        newRound.typingText(roundNum, newRound.GetTEXT_SLEEP_MEDIUM_SLOW());
-        Random rand = new Random();
-        int prevNum = -1;
+				+ "                                             " + diff + "  LEVEL\n"
+				+ "---------------------------------------------------------------------------------------------------------\n";
+		UIHelper newRound = new UIHelper();
+		newRound.typingText(roundNum, newRound.GetTEXT_SLEEP_MEDIUM_SLOW());
+		Random rand = new Random();
+		ArrayList<Integer> previousNum = new ArrayList<>();
+		for (int i = 0; i < QUESTIONS_PER_ROUND; i++) {
+			int questionNumber = rand.nextInt(Questions.size() - 1);
+			if (isEqual(previousNum, questionNumber) == false) {
+				Question q = Questions.get(questionNumber);
+				printQuestionNumber(i + 1);
+				questionMaker maker = new questionMaker(q.getDifficulty(), q.getCategory(), q.getQuestion(),
+						q.getOptiona(), q.getOptionb(), q.getOptionc(), q.getOptiond(), q.getAnswer());
+				score = maker.makeQuestion(score, scanner);
+				previousNum.add(questionNumber);
+			} else {i--;}
 
-        for (int i = 0; i < QUESTIONS_PER_ROUND; i++) {
-            int questionNumber;
-            do {
-                questionNumber = rand.nextInt(MAXIMUM_QUESTIONS);
-            } while (questionNumber == prevNum);
+		}
 
-            Question q = Questions.get(questionNumber);
-            printQuestionNumber(i + 1);
-            questionMaker maker = new questionMaker(q.getDifficulty(), q.getCategory(), q.getQuestion(),
-                    q.getOptiona(), q.getOptionb(), q.getOptionc(), q.getOptiond(), q.getAnswer());
-            score = maker.makeQuestion(score,scanner);
+		/*
+		 * int prevNum = -1; for (int i = 0; i < QUESTIONS_PER_ROUND; i++) { int
+		 * questionNumber; do {questionNumber = rand.nextInt(MAXIMUM_QUESTIONS); } while
+		 * (questionNumber == prevNum);
+		 * 
+		 * Question q = Questions.get(questionNumber); printQuestionNumber(i + 1);
+		 * questionMaker maker = new questionMaker(q.getDifficulty(), q.getCategory(),
+		 * q.getQuestion(), q.getOptiona(), q.getOptionb(), q.getOptionc(),
+		 * q.getOptiond(), q.getAnswer()); score = maker.makeQuestion(score,scanner);
+		 * 
+		 * prevNum = questionNumber; }
+		 */
 
-            prevNum = questionNumber;
-        }
-
-        return score;
+		return score;
 	}
 
 	public void printQuestionNumber(int i) {
@@ -46,9 +65,12 @@ public class RandomQuestions {
 		ArrayList<Question> Questions = readQuestions.readQuestions();
 		RandomQuestions newQ = new RandomQuestions();
 		SplitArrays newArray = new SplitArrays();
-		score = newQ.RondomiseQuesionts(score, Difficulty.NOVICE, newArray.SplitedArray(Questions, Difficulty.NOVICE),scanner);
-		score = newQ.RondomiseQuesionts(score, Difficulty.INTERMEDIATE, newArray.SplitedArray(Questions, Difficulty.INTERMEDIATE),scanner);
-		score = newQ.RondomiseQuesionts(score, Difficulty.EXPERT, newArray.SplitedArray(Questions, Difficulty.EXPERT),scanner);
+		score = newQ.RondomiseQuesionts(score, Difficulty.NOVICE, newArray.SplitedArray(Questions, Difficulty.NOVICE),
+				scanner);
+		score = newQ.RondomiseQuesionts(score, Difficulty.INTERMEDIATE,
+				newArray.SplitedArray(Questions, Difficulty.INTERMEDIATE), scanner);
+		score = newQ.RondomiseQuesionts(score, Difficulty.EXPERT, newArray.SplitedArray(Questions, Difficulty.EXPERT),
+				scanner);
 		return score;
 	}
 
